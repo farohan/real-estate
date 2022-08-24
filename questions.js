dae87f96430f4d5a0d805460a2bad15a4e9cbea6
farohan <68710283+farohan@users.noreply.github.com>
2022-08-23 18:50:51 -0700

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,93 @@
+//Welcome to questions.js!
+
+class Question {
+    constructor(q, q0, q1, q2, q3) {
+        this.q = q;
+        this.q0 = q0;
+        this.q1 = q1;
+        this.q2 = q2;
+        this.q3 = q3;
+    }
+}
+
+const one = new Question(
+    '1 Real Estate Quiz Question Goes Here.',
+    'Option A',
+    'Option B',
+    'Option C', 
+    'Option D'
+);
+
+const two = new Question(
+    '2 Real Estate Quiz Question Goes Here.',
+    'Option A',
+    'Option B',
+    'Option C', 
+    'Option D'
+);
+
+const three = new Question(
+    '3 Real Estate Quiz Question Goes Here.',
+    'Option A',
+    'Option B',
+    'Option C', 
+    'Option D'
+);
+
+const four = new Question(
+    '4 Real Estate Quiz Question Goes Here.',
+    'Option A',
+    'Option B',
+    'Option C', 
+    'Option D'
+);
+
+const five = new Question(
+    '5 Real Estate Quiz Question Goes Here.',
+    'Option A',
+    'Option B',
+    'Option C', 
+    'Option D'
+);
+
+const six = new Question(
+    '6 Real Estate Quiz Question Goes Here.',
+    'Option A',
+    'Option B',
+    'Option C', 
+    'Option D'
+);
+
+const seven = new Question(
+    '7 Real Estate Quiz Question Goes Here.',
+    'Option A',
+    'Option B',
+    'Option C', 
+    'Option D'
+);
+
+const eight = new Question(
+    '8 Real Estate Quiz Question Goes Here.',
+    'Option A',
+    'Option B',
+    'Option C', 
+    'Option D'
+);
+
+const nine = new Question(
+    '9 Real Estate Quiz Question Goes Here.',
+    'Option A',
+    'Option B',
+    'Option C', 
+    'Option D'
+);
+
+const ten = new Question(
+    '10 Real Estate Quiz Question Goes Here.',
+    'Option A',
+    'Option B',
+    'Option C', 
+    'Option D'
+);
+
+export const allQuestions = [one, two, three, four, five, six, seven, eight, nine, ten];
